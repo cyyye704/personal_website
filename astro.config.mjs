b48ebbc1d518abcbb0cd055ec78ba06230ab992b
cyyye704 <ycy0704@outlook.com>
2026-06-08 23:38:26 +0800

    Configure Astro custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'http://cyyye704.github.io',
-  base: '/personal_website',
+  site: "https://chenyiyue.com",
 });
